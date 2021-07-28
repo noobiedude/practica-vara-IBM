744cb173dbd8c789b1fdec58f15afdfa62c8f2b5
Lorena Pintea <66164602+LorenaPintea@users.noreply.github.com>
2021-07-28 21:31:31 +0300

    small update
    -am adaugat authRoutes + cookieParser

--- backend/index.js
+++ backend/index.js
@@ -4,13 +4,15 @@ const cors = require('cors');
 const databaseConfig = require(`./config/dbConfig`);
 const CommentModel = require('./models/CommentModel');
 const PostModel = require('./models/PostModel');
+const UserModel = require('./models/UserModel');
 require(`dotenv`).config({path: __dirname + `/.env`});
 const FRONTEND_ADDRESS = `http://localhost:3000`;
 const PORT = process.env.PORT || 5000;
 const postRoutes = require(`./routes/post`);
+const authRoutes = require(`./routes/auth`);
+const cookieParser = require('cookie-parser');
 
 app.use(express.json());
-
 
 app.use(cors({
     origin: FRONTEND_ADDRESS,
@@ -18,8 +20,8 @@ app.use(cors({
   }));
 
 app.use(`/posts`, postRoutes);
-
-
+app.use(authRoutes);
+app.use(cookieParser);
 
 databaseConfig.connectToDatabase();
 
