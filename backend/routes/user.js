e49de89cfc4d4bfbadb42b470563a54a27603e97
Lorena Pintea <66164602+LorenaPintea@users.noreply.github.com>
2021-08-08 13:19:56 +0300

    Update user.js

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -8,6 +8,6 @@ const router = express.Router();
 router.get(`/users`, getUsers);
 router.get(`/user/:id`, requireAuth, getUser);
 router.put(`/update/:id/`, requireAuth, userPermission, updateUser);
-router.delete(`/delete/:id`, requireAuth, userPermission, deleteUser);
+router.delete(`/delete`, requireAuth, userPermission, deleteUser);
 router.get(`/users/profilePicture`, getProfilePicture);
 module.exports = router;
