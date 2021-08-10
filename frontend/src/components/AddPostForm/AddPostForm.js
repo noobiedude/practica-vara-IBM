d48c5db9f57d5e005872886ed5b72cfe74b11c50
AlexandraIanus <59911710+IanusAlexandra@users.noreply.github.com>
2021-08-10 21:13:57 +0300

    Partial working post functionality

--- frontend/src/components/AddPostForm/AddPostForm.js
+++ frontend/src/components/AddPostForm/AddPostForm.js
@@ -1,65 +1,137 @@
-import { useFormik } from "formik";
-import React from "react";
+import { useFormik, withFormik } from "formik";
+import React, { useState } from "react";
 import * as Yup from "yup";
 import axios from "../../axios/axios";
-
+import { useHistory } from "react-router-dom";
 import "./addForm.scss";
 
+
+
 function AddPostForm () {
-  
+ 
+ 
+  const [selectWork,setSelectWork]=useState('');
+ 
+  let history = useHistory();
       const formik =useFormik({
-        initialValues:{}
+        initialValues:{
+      title:"",
+      description:"",
+      location:"",
+      programmingLanguage:"",
+
+        }, validationSchema: Yup.object({
+          title: Yup.string()
+            .max(100, "Must be 100 characters or less")
+            .min(2, "Must be 2 characters or more")
+            .required("Required"),
+          description: Yup.string()
+            .max(1500, "Must be 1500 characters or less")
+            .min(30, "Must be 30 characters or more")
+            .required("Required"),
+            location: Yup.string()
+            .max(100, "Must be 100 characters or less")
+            .min(2, "Must be 2 characters or more")
+            .required("Required"),
+            programmingLanguage: Yup.string()
+            .max(100, "Must be 100 characters or less")
+            .min(2, "Must be 2 characters or more")
+            .required("Required"),
+          
+        }),
+        onSubmit:(values)=>{
+          // console.log(values)
+          // const user=JSON.parse(window.localStorage.getItem("user"))
+          
+          let post=values;
+          post.workHours= selectWork;
+         
+          
+          axios.post("/posts/addPost",post).then((response)=>{
+            console.log(post)
+            let posts=[]
+            posts=response.data.posts;
+            posts.push(post)
+            console.log(posts)
+            
+            if(response.data.posts){
+              localStorage.setItem("post", JSON.stringify(posts));
+
+            }
+            history.push({ pathname: "/feed" });
+            window.location.reload();
+          })
+          .catch((err)=>{
+            console.log(err);
+          })
+        }
       })
-        
+     
       return (
       <div className="App">
     
     <div className="add-box">
       <h2>Add a post</h2>
-      <form>
-    <div className="user-box box">
-         <select name="type" id="type">
-         <option disabled selected value>Type select...</option>
-           <option value="">Offer</option>
-           <option value="">Request</option>
-           </select>
-           <label className="user-box"> Type: </label>
-    </div>
+      <form onSubmit={formik.handleSubmit}>
         <div className="user-box">
-        <input name="title" id="title" type="text" />
+        <input onChange={formik.handleChange} value={formik.values.title} name="title" id="title" type="text" onBlur={formik.handleBlur}
+            error={formik.errors.name && formik.touched.name ? true : false}
+            helperText={
+              formik.errors.name && formik.touched.name
+                ? formik.errors.name
+                : ""
+            } />
         <label>Title: </label>
           </div>
          <div className="user-box">
-         <input name="description" id="description" type="text" />
+         <input onChange={formik.handleChange} value={formik.values.description} name="description" id="description" type="text" onBlur={formik.handleBlur}
+            error={formik.errors.name && formik.touched.name ? true : false}
+            helperText={
+              formik.errors.name && formik.touched.name
+                ? formik.errors.name
+                : ""
+            } />
           <label >Description</label>
         </div>
 
         <div className="user-box box">
-         <select name="workHours" id="workHours">
-         <option value=""  disabled selected value>Work hours select...</option>
-           <option value="">Full-time</option>
-           <option value="">Part-time</option>
+         <select  onChange={(e) => setSelectWork(e.target.value)}name="workHours" id="workHours" >
+         <option   selected disabled hidden>Work hours select...</option>
+           <option value="Full-time">Full-time</option>
+           <option value="Part-time">Part-time</option>
            </select>
            <label className="user-box"> Work hours: </label>
     </div>
 
     <div className="user-box">
-         <input name="location" id="location" type="text" />
+         <input onChange={formik.handleChange} value={formik.values.location} name="location" id="location" type="text" onBlur={formik.handleBlur}
+            error={formik.errors.name && formik.touched.name ? true : false}
+            helperText={
+              formik.errors.name && formik.touched.name
+                ? formik.errors.name
+                : ""
+            } />
           <label >Location</label>
     </div>
 
     <div className="user-box" >
-         <input name="programmingLanguage" id="programmingLanguage" type="text" />
+         <input onChange={formik.handleChange} value={formik.values.programmingLanguage} name="programmingLanguage" id="programmingLanguage" type="text" onBlur={formik.handleBlur}
+            error={formik.errors.name && formik.touched.name ? true : false}
+            helperText={
+              formik.errors.name && formik.touched.name
+                ? formik.errors.name
+                : ""
+            }/>
           <label >Programming Language</label>
         </div>
 
-        <a href="#">
+        <button  type="submit">
       <span></span>
       <span></span>
       <span></span>
       <span></span>
       Submit
-    </a>
+    </button>
       </form>
     </div>
 
