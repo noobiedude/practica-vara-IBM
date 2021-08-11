eaccd2368ef1278448d6415d563db396e396cb64
CatalinNedelea <69271126+CatalinNedelea@users.noreply.github.com>
2021-08-11 21:12:29 +0300

    Changed some style, AddPost button
    Changed the style for some items, re-placed the AddPost button, removed AddPost button if not logged in

--- frontend/src/components/Feed/Feed.js
+++ frontend/src/components/Feed/Feed.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react'
 import Post from '../Post/Post'
-import User from '../User/User'
+import { NavLink } from 'react-router-dom'
 import './Feed.scss'
-import FlipMove from 'react-flip-move'
 import axios from '../../axios/axios'
 import { v4 as uuidv4 } from 'uuid'
 import Filter from '../Filter/Filter'
+import { Button } from '@material-ui/core'
 
 function Feed () {
   const [error, setError] = useState(false)
@@ -19,38 +19,23 @@ function Feed () {
   const [location, setLocation] = useState('')
   const [filter, setFilter] = useState({})
 
-  // function onChange (nameValue, value) {
-  //   if (nameValue === 'programmingLanguage') setProgrammingLanguage(value)
-  //   if (nameValue === 'workHours') setWorkHours(value)
-  //   if (nameValue === 'type') setType(value)
-  //   if (nameValue === 'location') setLocation(value)
-  // }
-
   const removePost = postRemoved => {
     axios
       .delete('/posts/' + postRemoved?._id)
       .then(deleted => {
         console.log(deleted.data)
         console.log(deleted)
-        axios
-          .post(
-            '/posts',
-            { lastPostId },
-            {
-              params: filter
-            }
-          )
-          .then(response => {
-            if (response.data.lastPostId === undefined) setHasMorePosts(false)
-            //console.log(response.data);
-            setData([...data, ...response.data?.posts])
-            setLastPostId(response.data.lastPostId)
-            window.location.reload()
-          })
+        axios.post('/posts', { lastPostId }).then(response => {
+          if (response.data.lastPostId === undefined) setHasMorePosts(false)
+          //console.log(response.data);
+          setData([...data, ...response.data?.posts])
+          setLastPostId(response.data.lastPostId)
+          window.location.reload()
+        })
       })
       .catch(error => {
         console.log(error)
-        alert("Your type of user cannot delete this post!")
+        alert('Your type of user cannot delete this post!')
       })
   }
 
@@ -64,16 +49,10 @@ function Feed () {
   function getData () {
     if (hasMorePosts) {
       axios
-        .post(
-          '/posts',
-          { lastPostId },
-          {
-            params: filter
-          }
-        )
+        .post('/posts', { lastPostId })
         .then(response => {
           if (response.data.lastPostId === undefined) setHasMorePosts(false)
-          //console.log(response.data);
+          console.log(response.data)
 
           setData([...data, ...response.data?.posts])
 
@@ -84,14 +63,6 @@ function Feed () {
         })
     }
   }
-
-  // useEffect(() => {
-  //   setFilter(prevState => ({
-  //     ...prevState,
-  //     pl: programmingLanguage
-  //   }))
-  //   console.log(filter)
-  // }, [programmingLanguage])
 
   const firstEvent = e => {
     if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
@@ -107,17 +78,55 @@ function Feed () {
       window.removeEventListener(`scroll`, firstEvent)
     }
   }, [])
+  const filtData = data
+    .filter(elem => {
+      if (programmingLanguage.length === 0) return true
+      return elem.programmingLanguage
+        .toLowerCase()
+        .includes(programmingLanguage.toLowerCase())
+    })
+    .filter(elem => {
+      if (location.length === 0) return true
+      return elem.location.toLowerCase().includes(location.toLowerCase())
+    })
+    .filter(elem => {
+      if (workHours.length === 0) return true
+      return elem.workHours.toLowerCase().includes(workHours.toLowerCase())
+    })
+    .filter(elem => {
+      if (type.length === 0) return true
+      return elem.type.toLowerCase().includes(type.toLowerCase())
+    })
+
+  const currentUser = JSON.parse(localStorage.getItem('user'))
 
   return (
     <div>
-      <Filter
-        location={location}
-        type={type}
-        programmingLanguage={programmingLanguage}
-        workHours={workHours}
-        setProgrammingLanguage={setProgrammingLanguage}
-      />
-      {data.map(item => {
+      {console.log(filtData)}
+      <div className='specialElems'>
+        <Filter
+          location={location}
+          type={type}
+          programmingLanguage={programmingLanguage}
+          workHours={workHours}
+          setProgrammingLanguage={setProgrammingLanguage}
+          setLocation={setLocation}
+          setType={setType}
+          setWorkHours={setWorkHours}
+        />
+        {currentUser ? (
+          <NavLink
+            to={`/addPost`}
+            style={{ textDecoration: 'none', color: '5F9EA0' }}
+          >
+            <Button variant='outlined' style={{ color: 'black' }}>
+              Add Post
+            </Button>
+          </NavLink>
+        ) : null}
+      </div>
+
+      {filtData.map(item => {
         return (
           <div key={uuidv4()}>
             <Post post={item} onRemovePost={removePost} />{' '}
