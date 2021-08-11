3190a02529a4704b0c5d75d970bd7f2331928187
CatalinNedelea <69271126+CatalinNedelea@users.noreply.github.com>
2021-08-11 21:10:19 +0300

    Small changes and code cleanup

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,8 +1,7 @@
-import Login from "../Login/Login";
-import Home from "../Home/Home";
-import Feed from "../Feed/Feed";
-import { NavLink } from "react-router-dom";
-import { Switch, Route, useHistory } from "react-router-dom";
+import Home from '../Home/Home'
+import Feed from '../Feed/Feed'
+import { NavLink } from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
 import {
   AppBar,
   Toolbar,
@@ -12,171 +11,173 @@ import {
   IconButton,
   Drawer,
   Link,
-  MenuItem,
-} from "@material-ui/core";
-import MenuIcon from "@material-ui/icons/Menu";
-import React, { useState, useEffect } from "react";
-import { Link as RouterLink } from "react-router-dom";
-
+  MenuItem
+} from '@material-ui/core'
+import MenuIcon from '@material-ui/icons/Menu'
+import React, { useState, useEffect } from 'react'
+import { Link as RouterLink } from 'react-router-dom'
+import './Header.scss'
 
 const headersData = [
   {
-    label: "Home",
-    href: "/",
-    component: Home,
+    label: 'Home',
+    href: '/',
+    component: Home
   },
   {
-    label: "Jobs",
-    href: "/feed",
-    component: Feed,
-  },
-];
+    label: 'Jobs',
+    href: '/feed',
+    component: Feed
+  }
+]
 
 const useStyles = makeStyles(() => ({
   root: {
-    flexGrow: 1,
+    flexGrow: 1
   },
   container: {
-    alignItems: "flex-end",
-    justifyContent: "space-between",
+    alignItems: 'flex-end',
+    justifyContent: 'space-between'
   },
   link: {
-    color: "inherit",
-    textDecoration: "none",
+    color: 'inherit',
+    textDecoration: 'none'
   },
   header: {
-    backgroundColor: "#5F9EA0",
-    paddingRight: "79px",
-    paddingLeft: "118px",
-    "@media (max-width: 900px)": {
-      paddingLeft: 0,
-    },
+    backgroundColor: '#5F9EA0',
+    paddingRight: '79px',
+    paddingLeft: '118px',
+    '@media (max-width: 900px)': {
+      paddingLeft: 0
+    }
   },
   logo: {
-    fontFamily: "Monserrat",
+    fontFamily: 'Monserrat',
     fontWeight: 300,
-    color: "#FFFEFE",
-    textAlign: "left",
+    color: '#FFFEFE',
+    textAlign: 'left'
   },
   Login: {
-    fontFamily: "Monserrat",
+    fontFamily: 'Monserrat',
     fontWeight: 300,
-    color: "#FFFEFE",
+    color: '#FFFEFE'
   },
   logout: {
-    fontFamily: "Monserrat",
+    fontFamily: 'Monserrat',
     fontWeight: 300,
-    color: "#FFFEFE",
+    color: '#FFFEFE'
   },
   menuButton: {
-    fontFamily: "Monserrat",
+    fontFamily: 'Monserrat',
     fontWeight: 300,
-    size: "18px",
-    marginLeft: "38px",
+    size: '18px',
+    marginLeft: '38px'
   },
   toolbar: {
-    display: "flex",
-    justifyContent: "space-between",
+    display: 'flex',
+    justifyContent: 'space-between'
+  },
+  menuButtonSection: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center'
   },
   drawerContainer: {
-    padding: "20px 30px",
-    display:"flex",
-    flexDirection:"column",
-    alignItems: "center",
-    justifyContent: "space-around",
-  },
-}));
+    padding: '20px 30px',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'space-around'
+  }
+}))
 
-export default function Header() {
-  const { header, logo, menuButton, toolbar, drawerContainer } = useStyles();
+export default function Header () {
+  const { header, logo, menuButton, toolbar, drawerContainer } = useStyles()
 
   const [state, setState] = useState({
     mobileView: false,
-    drawerOpen: false,
-  });
+    drawerOpen: false
+  })
 
-  const { mobileView, drawerOpen } = state;
+  const { mobileView, drawerOpen } = state
 
   useEffect(() => {
     const setResponsiveness = () => {
       return window.innerWidth < 900
-        ? setState((prevState) => ({ ...prevState, mobileView: true }))
-        : setState((prevState) => ({ ...prevState, mobileView: false }));
-    };
+        ? setState(prevState => ({ ...prevState, mobileView: true }))
+        : setState(prevState => ({ ...prevState, mobileView: false }))
+    }
 
-    setResponsiveness();
+    setResponsiveness()
 
-    window.addEventListener("resize", () => setResponsiveness());
+    window.addEventListener('resize', () => setResponsiveness())
 
     return () => {
-      window.removeEventListener("resize", () => setResponsiveness());
-    };
-  }, []);
+      window.removeEventListener('resize', () => setResponsiveness())
+    }
+  }, [])
 
   const DisplayDesktop = () => {
-    const currentUser = JSON.parse(localStorage.getItem("user"));
-    let history = useHistory();
+    const currentUser = JSON.parse(localStorage.getItem('user'))
+    let history = useHistory()
 
     const logout = () => {
-      localStorage.removeItem("user");
-      history.push({ pathname: "/" });
-      window.location.reload();
-    };
+      localStorage.removeItem('user')
+      history.push({ pathname: '/' })
+      window.location.reload()
+    }
 
     return (
       <Toolbar className={toolbar}>
         {hireStudLogo}
         <div>{getMenuButtons()}</div>
-        <div style={{ color: "inherit" }}>
+        <div style={{ color: 'inherit' }}>
           {!currentUser ? (
-            <NavLink to={`/login`} style={{ textDecoration: "none" }}>
-              <Button style={{ color: "#ADFF2F" }}>Login</Button>
+            <NavLink to={`/login`} style={{ textDecoration: 'none' }}>
+              <Button style={{ color: '#ADFF2F' }}>Login</Button>
             </NavLink>
           ) : (
             <div>
-            <NavLink to={`/addPost`} style={{ textDecoration: "none", color: "5F9EA0" }}>
-               <Button disabled style={{ color: "white" }}>
-                Add Post
-               </Button>
-             </NavLink>
-              <NavLink to={`/userprofile`} style={{ textDecoration: "none" }}>
-                <Button disabled style={{ color: "white" }}>
+              <NavLink to={`/userprofile`} style={{ textDecoration: 'none' }}>
+                <Button disabled style={{ color: 'white' }}>
                   {currentUser.name}
                 </Button>
               </NavLink>
-              <Button onClick={logout} style={{ color: "#ADFF2F" }}>
+
+              <Button onClick={logout} style={{ color: '#ADFF2F' }}>
                 Logout
               </Button>
             </div>
           )}
         </div>
       </Toolbar>
-    );
-  };
+    )
+  }
 
   const DisplayMobile = () => {
     const handleDrawerOpen = () =>
-      setState((prevState) => ({ ...prevState, drawerOpen: true }));
+      setState(prevState => ({ ...prevState, drawerOpen: true }))
     const handleDrawerClose = () =>
-      setState((prevState) => ({ ...prevState, drawerOpen: false }));
-    const currentUser = JSON.parse(localStorage.getItem("user"));
-    let history = useHistory();
+      setState(prevState => ({ ...prevState, drawerOpen: false }))
+    const currentUser = JSON.parse(localStorage.getItem('user'))
+    let history = useHistory()
 
     const logout = () => {
-      localStorage.removeItem("user");
-      history.push({ pathname: "/" });
-      window.location.reload();
-    };
+      localStorage.removeItem('user')
+      history.push({ pathname: '/' })
+      window.location.reload()
+    }
 
     return (
       <Toolbar>
         <IconButton
           {...{
-            edge: "start",
-            color: "inherit",
-            "aria-label": "menu",
-            "aria-haspopup": "true",
-            onClick: handleDrawerOpen,
+            edge: 'start',
+            color: 'inherit',
+            'aria-label': 'menu',
+            'aria-haspopup': 'true',
+            onClick: handleDrawerOpen
           }}
         >
           <MenuIcon />
@@ -184,74 +185,42 @@ export default function Header() {
 
         <Drawer
           {...{
-            anchor: "left",
+            anchor: 'left',
             open: drawerOpen,
-            onClose: handleDrawerClose,
+            onClose: handleDrawerClose
           }}
         >
-          <div className={drawerContainer}>{getDrawerChoices()}
-          <div style={{ color: "inherit" }}>
-          {!currentUser ? (
-            <NavLink to={`/login`} style={{ textDecoration: "none" }}>
-              <Button style={{ color: "#ADFF2F"}}>
-                Login
-              </Button>
-            </NavLink>
-          ) : (
-            <div>
-            <NavLink to={`/addPost`} style={{ textDecoration: "none", color: "5F9EA0" }}>
-               
-               <Button disabled style={{ color: "5F9EA0" }}>
-                Add Post
-               </Button>
-             </NavLink>
-              <NavLink to={`/userprofile`} style={{ textDecoration: "none", color: "5F9EA0" }}>
-               
-                <Button disabled style={{ color: "5F9EA0" }}>
-                  {currentUser.name}
-                </Button>
-              </NavLink>
-              
-              <Button
-                onClick={logout}
-                style={{ color: "#ADFF2F" }}
-              >
-                Logout
-              </Button>
+          <div className={drawerContainer}>
+            {getDrawerChoices()}
+            <div style={{ color: 'inherit' }}>
+              {!currentUser ? (
+                <NavLink to={`/login`} style={{ textDecoration: 'none' }}>
+                  <Button style={{ color: '#ADFF2F' }}>Login</Button>
+                </NavLink>
+              ) : (
+                <div>
+                  <NavLink
+                    to={`/userprofile`}
+                    style={{ textDecoration: 'none', color: '5F9EA0' }}
+                  >
+                    <Button disabled style={{ color: '5F9EA0' }}>
+                      {currentUser.name}
+                    </Button>
+                  </NavLink>
+
+                  <Button onClick={logout} style={{ color: '#ADFF2F' }}>
+                    Logout
+                  </Button>
+                </div>
+              )}
             </div>
-          )}
-        </div>
-        </div>
+          </div>
         </Drawer>
 
         <div>{hireStudLogo}</div>
-
-        {/* <div style={{ color: "inherit" }}>
-          {!currentUser ? (
-            <NavLink to={`/login`} style={{ textDecoration: "none" }}>
-              <Button style={{ color: "#ADFF2F", position: "right" }}>
-                Login
-              </Button>
-            </NavLink>
-          ) : (
-            <div>
-              <NavLink to={`/userprofile`} style={{ textDecoration: "none" }}>
-                <Button disabled style={{ color: "white" }}>
-                  {currentUser.name}
-                </Button>
-              </NavLink>
-              <Button
-                onClick={logout}
-                style={{ color: "#ADFF2F", justifyContent: "space-between" }}
-              >
-                Logout
-              </Button>
-            </div>
-          )}
-        </div> */}
       </Toolbar>
-    );
-  };
+    )
+  }
 
   const getDrawerChoices = () => {
     return headersData.map(({ label, href, component }) => {
@@ -260,24 +229,24 @@ export default function Header() {
           {...{
             component: RouterLink,
             to: href,
-            color: "inherit",
-            style: { textDecoration: "none" },
-            key: label,
+            color: 'inherit',
+            style: { textDecoration: 'none' },
+            key: label
           }}
         >
           <MenuItem>{label}</MenuItem>
         </Link>
-      );
-    });
-  };
+      )
+    })
+  }
 
   const hireStudLogo = (
-    <Typography variant="h6" component="h1" className={logo}>
-      <a href="/" style={{ color: "inherit" , textDecoration: "none" }}>
-        HireStud{" "}
+    <Typography variant='h6' component='h1' className={logo}>
+      <a href='/' style={{ color: 'inherit', textDecoration: 'none' }}>
+        HireStud{' '}
       </a>
     </Typography>
-  );
+  )
 
   const getMenuButtons = () => {
     return headersData.map(({ label, href, component }) => {
@@ -285,17 +254,17 @@ export default function Header() {
         <Button
           {...{
             key: label,
-            color: "inherit",
+            color: 'inherit',
             to: href,
             component: RouterLink,
-            className: menuButton,
+            className: menuButton
           }}
         >
           {label}
         </Button>
-      );
-    });
-  };
+      )
+    })
+  }
 
   return (
     <header>
@@ -303,5 +272,5 @@ export default function Header() {
         {mobileView ? DisplayMobile() : DisplayDesktop()}
       </AppBar>
     </header>
-  );
+  )
 }
