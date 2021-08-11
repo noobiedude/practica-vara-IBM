5cb5225f25927445a2360cb1c43650e2b6a6b1e1
CatalinNedelea <69271126+CatalinNedelea@users.noreply.github.com>
2021-08-11 14:31:29 +0300

    Remove Post stuff

--- frontend/src/components/Post/Post.js
+++ frontend/src/components/Post/Post.js
@@ -1,56 +1,145 @@
-import React, { useState, useEffect } from "react";
-import { NavLink } from "react-router-dom";
-import "./Post.scss";
-import User from "../User/User";
-import axios from "../../axios/axios";
-import VisibilityIcon from "@material-ui/icons/Visibility";
-import IconButton from "@material-ui/core/IconButton";
-import { Button } from "@material-ui/core";
+import React, { useState, useEffect } from 'react'
+import { NavLink } from 'react-router-dom'
+import './Post.scss'
+import User from '../User/User'
+import axios from '../../axios/axios'
+import VisibilityIcon from '@material-ui/icons/Visibility'
+import IconButton from '@material-ui/core/IconButton'
+import { Button } from '@material-ui/core'
+import Modal from '@material-ui/core/Modal'
+import { makeStyles } from '@material-ui/core/styles'
+import DeleteIcon from '@material-ui/icons/Delete'
 
-const Post = (props) => {
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: 230,
+    boxSizing: 'content-box',
+    justifyContent: 'space-between',
+    backgroundColor: 'ghostwhite',
+    marginBottom: 30
+  },
+  media: {
+    height: 0
+  },
+  link: {
+    color: 'inherit',
+    textDecoration: 'none'
+  },
+  paper: {
+    position: 'absolute',
+    width: '30%',
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+    color: 'black'
+  },
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  buttonsContainer: {
+    display: 'flex',
+    justifyContent: 'space-evenly'
+  },
+  cardHeaderRoot: {
+    overflow: 'hidden'
+  },
+  cardHeaderContent: {
+    overflow: 'hidden'
+  },
+  buttonContinue: {
+    color: 'green'
+  }
+}))
+
+const Post = props => {
+  const classes = useStyles()
+
+  const [open, setOpen] = React.useState(false)
+
+  const handleOpen = () => {
+    setOpen(true)
+  }
+
+  const handleClose = () => {
+    setOpen(false)
+  }
   return (
-    <div>
-      <div class="movie_card" >
-        <div class="info_section">
-          <div class="movie_header">
-            <img
-              class="locandina"
-              src="https://www.pngitem.com/pimgs/m/150-1503945_transparent-user-png-default-user-image-png-png.png"
-              alt="some alt stuff"
-            />
-            <h2>{props.post.title}</h2>
-            <h4>@{props.post.createdBy?.name}</h4>
-            <div class="minutes">
-              <div>{props.post.type}</div>
-              <div>{props.post.workHours}</div>
-              <div>{props.post.location}</div>
-              <div>{props.post.programmingLanguage}</div>
-              <div >{props.post.createdAt}</div>
+    <>
+      <div>
+        <div class='movie_card'>
+          <div class='info_section'>
+            <div class='movie_header'>
+              <img
+                class='locandina'
+                src='https://www.pngitem.com/pimgs/m/150-1503945_transparent-user-png-default-user-image-png-png.png'
+                alt='some alt stuff'
+              />
+              <h2>{props.post.title}</h2>
+              <h4>@{props.post.createdBy?.name}</h4>
+              <div class='minutes'>
+                <div>{props.post.type}</div>
+                <div>{props.post.workHours}</div>
+                <div>{props.post.location}</div>
+                <div>{props.post.programmingLanguage}</div>
+                <div>{props.post.createdAt}</div>
+              </div>
             </div>
-            {/* <p class="type">{props.post.createdAt}</p> */}
-          </div>
-          <div class="movie_desc">
-            <p class="text">{props.post.description}</p>
-          </div>
-          <div class="movie_social">
-            {/* <IconButton aria-label="add to favorites"> */}
-            <NavLink
-              to={`/posts/inspect/${props.post._id}`}
-              style={{ textDecoration: "none", color: "inherit" }}
-            >
-              <Button
-                variant="outlined"
-                size="small"
-                style={{ marginLeft: "17.5px" }}
+            <div class='movie_desc'>
+              <p class='text'>{props.post.description}</p>
+            </div>
+            <div class='movie_social'>
+              <NavLink
+                to={`/posts/inspect/${props.post._id}`}
+                style={{ textDecoration: 'none', color: 'inherit' }}
               >
-                See Full Post
-              </Button>
-            </NavLink>
+                <Button
+                  variant='outlined'
+                  size='small'
+                  style={{ marginLeft: '17.5px' }}
+                >
+                  See Full Post
+                </Button>
+              </NavLink>
+
+              <IconButton onClick={handleOpen} aria-label='share'>
+                <DeleteIcon />
+              </IconButton>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-};
 
-export default Post;
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby='simple-modal-title'
+        aria-describedby='simple-modal-description'
+        className={classes.modal}
+      >
+        <div className={classes.paper}>
+          <h3>Are you sure you want to do this?</h3>
+          <div className={classes.buttonsContainer}>
+            <Button onClick={handleClose} variant='outlined' color='secondary'>
+              Cancel
+            </Button>
+            <Button
+              variant='outlined'
+              className={classes.buttonContinue}
+              onClick={() => {
+                props.onRemovePost(props.post)
+                setOpen(false)
+              }}
+            >
+              Continue
+            </Button>
+          </div>
+        </div>
+      </Modal>
+    </>
+  )
+}
+
+export default Post
